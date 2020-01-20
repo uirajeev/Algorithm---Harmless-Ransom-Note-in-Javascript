23cff731b479afe90e32007a2d0f3f207cebea17
Rajeev Kumar Singh <rajeevkumar.web@gmail.com>
2020-01-20 19:41:52 +0000

    Simple Solution

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Problem : You have been given two strings. You have to find out whether you can make up the first string with the words present in the second string.
 
-// Solution: 1
+// Solution: 1 Simple Solution
 
 function harmlessRansom(noteText, magazineText) {
   var noteArr = noteText.trim().split(' ');
